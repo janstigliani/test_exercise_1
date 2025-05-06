976925ef91aa1d4034ef3f3f7bfd1b8971ed2c32
janstigliani <janstigli@gmail.com>
2025-05-06 09:30:30 +0200

    added line

--- test_exercise_1/Program.cs
+++ test_exercise_1/Program.cs
@@ -2,6 +2,8 @@
 {
     private static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World, Burn!");
+        string massi = "Hello, World!";
+        Console.WriteLine(massi);
     }
 }
